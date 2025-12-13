d2b9674e26daea20bb7493c6a3662e4f23e51918
bhavishkulal <bhavish.kulal777@gmail.com>
2025-12-13 21:32:46 +0530

    Delete model_list.py

--- model_list.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyAxlg-tn7CzkZ2lAoIGLXHlDBLJTrVRz-4")
-
-models = genai.list_models()
-
-for m in models:
-    print(m.name, "  |  supports generate_content:", "generateContent" in m.supported_generation_methods)
